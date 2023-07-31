51de3a608221f6a8872b66c951a695278da260d1
Jaga1999 <jagasiva1999@gmail.com>
2023-07-31 21:30:35 +0530

    Added another route and send json response

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,7 +11,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+type ResponseData struct {
+	Message string `json:"message"`
+}
+
+func getMethod(w http.ResponseWriter, r *http.Request) {
+	response := ResponseData{
+		Message: "Hi there, I love Golang!",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/get", getMethod)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
